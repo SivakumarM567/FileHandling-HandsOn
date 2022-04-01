cc1327dd88120873ff5afc6adf21f188b712d093
SivakumarM567 <shivakumar85249@gmail.com>
2022-04-01 12:12:04 +0530

    Solution

---
+++ FileHandling1/FileHandling/Program1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHandling2
+{
+    internal class Program1
+    {
+        static void Main(string[] args)
+        {
+            NetworkLog fileHandling = new NetworkLog();
+            fileHandling.ReadFile();
+            Success fileHandling1 = new Success();
+            fileHandling1.ReadFile();
+            Failed fileHandling2 = new Failed();
+            fileHandling2.ReadFile();
+            Missed fileHandling3 = new Missed();
+            fileHandling3.ReadFile();
+            Dialled fileHandling4 = new Dialled();
+            fileHandling4.ReadFile();
+            Console.Read();
+        }
+    }
+}
